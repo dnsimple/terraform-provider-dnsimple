9f06ab4f536eeb21e964eb5a8a400bdf5aeb8df5
Ivan Bakalov <ivan.bakalov@dnsimple.com>
2023-03-01 05:03:04 +0200

    Update test and release workflows (#82)
    * Update test and release workflows
    
    * Add a test to check for missing documentation
    * Add steps in the release process to include the registry manifest
    
    * Add generators to main.go and debug flag
    
    * Update resource documentation

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-providers/terraform-provider-dnsimple/dnsimple"
 )
 
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ./example/
+
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//<remove to run>go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
+		Debug: debugMode,
+
 		ProviderFunc: dnsimple.Provider})
 }
