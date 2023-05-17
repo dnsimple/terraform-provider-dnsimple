b8bddd46f145559c8fb74107289e5408a6bf67d9
Wilson Lin <125257432+wilson-dnsimple@users.noreply.github.com>
2023-05-17 10:43:00 +0200

    Fix error when no alternate names are provided for the Let's Encrypt certificate resource (#111)

--- internal/framework/resources/lets_encrypt_certificate_resource_test.go
+++ internal/framework/resources/lets_encrypt_certificate_resource_test.go
@@ -57,6 +57,46 @@ func TestAccLetsEncryptCertificateResource(t *testing.T) {
 	})
 }
 
+func TestAccLetsEncryptCertificateResource_NoAlternateNames(t *testing.T) {
+	if os.Getenv("DNSIMPLE_SANDBOX") != "false" {
+		t.Skip("DNSIMPLE_SANDBOX is not set to `false` (read in CONTRIBUTING.md how to run this test)")
+		return
+	}
+	resourceName := "dnsimple_lets_encrypt_certificate.test"
+
+	domainId := os.Getenv("DNSIMPLE_DOMAIN")
+	certName := os.Getenv("DNSIMPLE_CERTIFICATE_NAME")
+	certAutoRenew := os.Getenv("DNSIMPLE_CERTIFICATE_AUTO_RENEW") == "1"
+	certSigAlg := os.Getenv("DNSIMPLE_CERTIFICATE_SIGNATURE_ALGORITHM")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { test_utils.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckLetsEncryptCertificateResourceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLetsEncryptCertificateResourceConfig_NoAlternateNames(domainId, certAutoRenew, certName, certSigAlg),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "domain_id", domainId),
+					resource.TestCheckResourceAttr(resourceName, "name", certName),
+					resource.TestCheckResourceAttr(resourceName, "alternate_names.#", "0"),
+					resource.TestCheckResourceAttrSet(resourceName, "years"),
+					resource.TestCheckResourceAttrSet(resourceName, "state"),
+					resource.TestCheckResourceAttrSet(resourceName, "authority_identifier"),
+					resource.TestCheckResourceAttr(resourceName, "auto_renew", strconv.FormatBool(certAutoRenew)),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "csr"),
+					resource.TestCheckResourceAttr(resourceName, "signature_algorithm", certSigAlg),
+				),
+			},
+			// Update is a no-op
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
 // We cannot delete certificates from the server.
 func testAccCheckLetsEncryptCertificateResourceDestroy(state *terraform.State) error {
 	return nil
@@ -75,4 +115,14 @@ resource "dnsimple_lets_encrypt_certificate" "test" {
 	alternate_names = %[4]s
 	signature_algorithm = %[5]q
 }`, domainId, autoRenew, name, string(alternateNamesRaw), signatureAlgorithm)
+}
+
+func testAccLetsEncryptCertificateResourceConfig_NoAlternateNames(domainId string, autoRenew bool, name string, signatureAlgorithm string) string {
+	return fmt.Sprintf(`
+resource "dnsimple_lets_encrypt_certificate" "test" {
+	domain_id = %[1]q
+	auto_renew = %[2]t
+	name = %[3]q
+	signature_algorithm = %[4]q
+}`, domainId, autoRenew, name, signatureAlgorithm)
 }
