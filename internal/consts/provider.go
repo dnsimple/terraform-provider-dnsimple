57c812222b9d589ac08bb0c12532e0996c34533e
Stephen Nelson <stephen.nelson@dnsimple.com>
2023-08-23 21:26:14 -0400

    add registrant change state constants

--- internal/consts/provider.go
+++ internal/consts/provider.go
@@ -1,11 +1,16 @@
 package consts
 
 const (
-	BaseURLSandbox        = "https://api.sandbox.dnsimple.com"
-	DomainStateRegistered = "registered"
-	DomainStateHosted     = "hosted"
-	DomainStateNew        = "new"
-	DomainStateFailed     = "failed"
-	DomainStateCancelling = "cancelling"
-	DomainStateCancelled  = "cancelled"
+	BaseURLSandbox                  = "https://api.sandbox.dnsimple.com"
+	DomainStateRegistered           = "registered"
+	DomainStateHosted               = "hosted"
+	DomainStateNew                  = "new"
+	DomainStateFailed               = "failed"
+	DomainStateCancelling           = "cancelling"
+	DomainStateCancelled            = "cancelled"
+	RegistrantChangeStateNew        = "new"
+	RegistrantChangeStatePending    = "pending"
+	RegistrantChangeStateCompleted  = "completed"
+	RegistrantChangeStateCancelling = "cancelling"
+	RegistrantChangeStateCancelled  = "cancelled"
 )
