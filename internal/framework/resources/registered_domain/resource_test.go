9e25ea602d85e531195a47f59f6b927d713eea1d
Stephen Nelson <stephen.nelson@dnsimple.com>
2023-08-28 23:52:44 -0400

    add registered_domain with contact change to tests

--- internal/framework/resources/registered_domain/resource_test.go
+++ internal/framework/resources/registered_domain/resource_test.go
@@ -179,6 +179,31 @@ func TestAccRegisteredDomainResource_ImportedWithDomainOnly(t *testing.T) {
 	})
 }
 
+func TestAccRegisteredDomainResource_WithContactChange(t *testing.T) {
+	domainName := os.Getenv("DNSIMPLE_DOMAIN")
+	resourceName := "dnsimple_registered_domain.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheckRegisteredDomain(t) },
+		ProtoV6ProviderFactories: test_utils.TestAccProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				ResourceName:      resourceName,
+				Config:            testAccRegisteredDomainResourceConfig(domainName, 1234),
+				ImportStateId:     domainName,
+				ImportState:       true,
+				ImportStateVerify: false,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", domainName),
+					resource.TestCheckResourceAttr(resourceName, "state", "registered"),
+					resource.TestCheckResourceAttr(resourceName, "registrant_id", "5678"),
+					resource.TestCheckNoResourceAttr(resourceName, "domain_registration"),
+				),
+			},
+		},
+	})
+}
+
 func testAccPreCheckRegisteredDomain(t *testing.T) {
 	test_utils.TestAccPreCheck(t)
 	if os.Getenv("DNSIMPLE_CONTACT_ID") == "" {
