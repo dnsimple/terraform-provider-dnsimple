f36ae095d49ff2a8f9c654f147c2217ace55a7e6
Stephen Nelson <stephen.nelson@dnsimple.com>
2024-01-17 04:08:23 -0500

    update domain delegation tests

--- internal/framework/resources/domain_delegation_resource_test.go
+++ internal/framework/resources/domain_delegation_resource_test.go
@@ -27,8 +27,8 @@ func TestAccDomainDelegationResource(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 					resource.TestCheckResourceAttr(resourceName, "domain", domainId),
 					resource.TestCheckResourceAttr(resourceName, "name_servers.#", "2"),
-					resource.TestCheckResourceAttr(resourceName, "name_servers.0", "ns-998.awsdns-60.net"),
-					resource.TestCheckResourceAttr(resourceName, "name_servers.1", "ns-1556.awsdns-02.co.uk"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "name_servers.*", "ns-998.awsdns-60.net"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "name_servers.*", "ns-1556.awsdns-02.co.uk"),
 				),
 			},
 			{
